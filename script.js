da91b09bd25bbfa9effc2c68c5c4d04bda1b755e
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-04-12 13:24:21 -0400

    modified script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Part 1: Growing Plants
+const PI = 3.1415;
+const radius = 5;
+const area = (PI * radius * radius)
+
+console.log(area)
+
+const mimSpace = 0.8
+
+console.log(area / mimSpace)
+
+const plants = 20;
+
+const after1 = plants * 2;
+const after2 = after1 * 2;
+const after3 = after2 * 2;
+
+console.log(after1)
+console.log(after2)
